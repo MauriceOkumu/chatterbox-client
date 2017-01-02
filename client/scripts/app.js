b734b0cf5b0061ef5c00a0195baea54bb44595b4
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-02 12:39:13 -0800

    app.fetch specs passing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -14,18 +14,18 @@ app.send = (message) =>{
     }
   });
 };
-// app.fetch = (message) => {
-//   $.ajax({
-// 	type: 'GET',
-// 	data: JSON.stringify(message),
-// 	success: function (data) {
-// 	  console.log('this is the data sent....:  ', data);
-// 	},
-// 	error: function (data) {
-// 	  console.error('chatterbox: Failed to send message');
-// 	}
-//   });
-// };
+app.fetch = (message) => {
+  $.ajax({
+    type: 'GET',
+    data: JSON.stringify(message),
+    success: function (data) {
+      console.log('this is the data sent....:  ', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
 // app.clearMessages = () => {
 // 	$.ajax({
 // 	  url: 'https://api.parse.com/1/classes/messages',
