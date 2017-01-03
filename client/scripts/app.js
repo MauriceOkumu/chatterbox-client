83fc428e78238b09b1ec330b4aaf9713d3648e82
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-03 11:43:24 -0800

    Still tweaking

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,43 +1,98 @@
 // YOUR CODE HERE:
-let app = {};
-app.init = () =>{};
-app.send = (message) =>{
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    success: function (data) {
-      console.log('this is the data sent....:  ', data);
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message');
+
+
+app = {
+
+  server: 'https://api.parse.com/1/classes/messages/',
+
+  init: function() {
+    // Get username
+    app.username = window.location.search.substr(10);
+
+    app.onscreenMessages = {};
+
+    // cache some dom references
+    app.$text = $('#message');
+
+    app.fetch();
+    // setInterval (app.fetch.bind(app), 1000);
+
+    $('#send').on('submit', app.handleSubmit);
+  },
+
+  send: function(message) {
+    $.ajax({
+      type: 'POST',
+      url: app.server,
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(json) {
+        message.objectId = json.objectId;
+        app.fetch()
+        // app.displayMessage(message);
+      },
+      error: function (error) {
+        console.error('Error', error);
+      }
+    });
+  },
+
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: function(json) {
+        app.displayMessages(json.results);
+      },
+      error: function (error) {
+        console.error('Error', error);
+      }
+    });
+  },
+  
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var message = {
+      username: app.username,
+      text: app.$text.val(),
+      roomname: app.roomname || 'lobby'
+    };
+
+    // app.$text.val('');
+
+    app.send(message);
+  },
+
+  clearMessages: function() {
+    $('#chats').html('');
+  },
+
+  renderMessage: function(message) {
+    var $user = $('<div>', {class: 'user'}).text(message.username);
+    var $text = $('<div>', {class: 'text'}).text(message.text);
+    var $message = $('<div>', {class: 'chat', 'data-id': message.objectId }).append($user, $text);
+    return $message;
+  },
+
+  displayMessage: function(message) {
+    if (!app.onscreenMessages[message.objectId]) {
+      var $html = app.renderMessage(message);
+      $('#chats').prepend($html);
+      app.onscreenMessages[message.objectId] = true;
     }
-  });
-};
-app.fetch = (message) => {
-  $.ajax({
-    type: 'GET',
-    data: JSON.stringify(message),
-    success: function (data) {
-      console.log('this is the data sent....:  ', data);
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message');
+  },
+
+  displayMessages: function(messages) {
+    for (var i = messages.length; i > 0; i--) {
+      app.displayMessage(messages[i - 1]);
     }
-  });
+  },
+
+
+
 };
-// app.clearMessages = () => {
-// 	$.ajax({
-// 	  url: 'https://api.parse.com/1/classes/messages',
-// 	  type: 'DELETE',
-// 	  data: JSON.stringify(message),
-// 	  success: function (data) {
-// 	    console.log('this is the data sent....:  ', data);
-// 	  },
-// 	  error: function (data) {
-// 	    console.error('chatterbox: Failed to send message');
-// 	  }
-//   });
-// };
-// app.renderMessage = () =>{};
-// app.renderRoom = () =>{};
+app.init()
+
+
